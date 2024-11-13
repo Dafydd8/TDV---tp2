a35edc4b4f626a84e46fc6a23860108e19ce7adc
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2024-11-13 18:41:12 -0300

    First commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from railway_service import *
+import os
+
+############################################################
+# CREAR Y MOSTRAR EL GRAFO COMO SE PROPONE EN EL ENUNCIADO #
+############################################################
+#file_name = os.path.join(os.path.dirname(__file__), '../instances/toy_instance.json')
+file_name = os.path.join(os.path.dirname(__file__), '../instances/pilar_cabred_instance.json')
+data = load_instance(file_name)
+grafo, nodos_estacion = create_graph(data)
+#show_graph(grafo, data, nodos_estacion)
+
+######################################################
+# RESOLVER EL PROBLEMA Y MOSTRAR EL FLUJO RESULTANTE #
+######################################################
+circulacion = solve_circulacion(grafo)
+cost = get_cost(grafo, circulacion, nodos_estacion)
+print("Costo de la circulación: ", cost)
+show_flow(grafo, circulacion, data, nodos_estacion)
